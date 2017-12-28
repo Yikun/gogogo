7a8ddb10c8ec7bbcfecf26693769a9ff03a177e1
Yikun Jiang <yikunkero@gmail.com>
2017-12-28 16:11:52 +0800

    Add blueprint getter.
    $ bpget.exe -h
    Usage of bpget.exe:
      -service string
            The service name of OpenStack (nova, cinder...) (default "nova")
      -version string
            The version of service (pike, queens...) (default "queens")

---
+++ bpget/bp_get.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"encoding/csv"
+	"os"
+	"log"
+	"strings"
+	"fmt"
+	"time"
+	"flag"
+)
+
+type BluePrint struct {
+	Priority string
+	Title    string
+	Series   string
+	Url      string
+}
+
+func GetBlueprint(service string, version string) []*BluePrint {
+	baseURL := "https://blueprints.launchpad.net"
+	docURL := strings.Join([]string{baseURL, service, version}, "/")
+	fmt.Println("Donwload blueprints from: " + docURL)
+	doc, err := goquery.NewDocument(docURL)
+	if err != nil {
+		panic(err)
+	}
+	var l []*BluePrint
+	// Find the bp table in html
+	doc.Find("#speclisting tbody tr").Each(func(i int, bpRow *goquery.Selection) {
+		// Get col in every row
+		bpCol := bpRow.Find("td")
+		// Get priority
+		bp := new(BluePrint)
+		bp.Priority = bpCol.Eq(0).Find("span").Eq(1).Text()
+		// Get url and title
+		Name := bpCol.Eq(1).Find("a")
+		url, exist := Name.Attr("href")
+		if exist {
+			bp.Url = baseURL + url
+		}
+		title, exist := Name.Attr("title")
+		if exist {
+			bp.Title = title
+		}
+		// Get version
+		bp.Series = bpCol.Eq(5).Find("a").Text()
+		if bp.Series == "" {
+			bp.Series = "queens"
+		}
+		// append bp on l
+		l = append(l, bp)
+	})
+
+	return l
+}
+
+func getFileName(s string, v string) string {
+	t := time.Now().Format("2006_01_02_15_04_05")
+	fileName := strings.Join([]string{s, v, t}, "_")
+	return fileName + ".csv"
+}
+
+func main() {
+	servicePtr := flag.String("service", "nova", "The service name of OpenStack (nova, cinder...)")
+	versionPtr := flag.String("version", "queens", "The version of service (pike, queens...)")
+	flag.Parse()
+	service := *servicePtr
+	version := *versionPtr
+	bps := GetBlueprint(service, version)
+	records := [][]string{
+		{"Blueprint", "Priority", "Link", "Catagory", "Tag"},
+	}
+	for _, bp := range bps {
+		rec := []string{bp.Title, bp.Priority, bp.Url, "新特性", bp.Series}
+		records = append(records, rec)
+	}
+
+	// Write csv to std
+	// w := csv.NewWriter(os.Stdout)
+	// w.WriteAll(records)
+
+	// Write csv to file
+	fileName := getFileName(service, version)
+	f, err := os.Create(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	f.WriteString("\xEF\xBB\xBF")
+	fw := csv.NewWriter(f)
+	fw.WriteAll(records)
+	if err := fw.Error(); err != nil {
+		log.Fatalln("error writing csv:", err)
+	}
+	fmt.Println("OK, download complete: " + fileName)
+}
